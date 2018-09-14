c0ddd7d110472a859a7e2bf18523f41f7cd63d18
acintosh <g1731057@m.sojo-u.ac.jp>
2018-09-14 19:06:30 +0900

    typo filename

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 git clone https://git.pleroma.social/pleroma/pleroma.git
 \cp -rf pleroma/config .
 \cp -rf pleroma/priv .
-sudo docker-compose -f ./docker-compose-setup.sh build
-sudo docker-compose -f ./docker-compose-setup.sh run --rm app mix generate_config
+sudo docker-compose -f ./docker-compose-setup.yml build
+sudo docker-compose -f ./docker-compose-setup.yml run --rm app mix generate_config
 \cp -f config/generated_config.exs config/prod.secret.exs
 nvim config/prod.secret.exs docker-compose.yml
 sudo docker-compose run --rm app mix ecto.migrate
